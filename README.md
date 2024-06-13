871b9d4fde79de1b0be47f80e555d0aae7985255
linweiHuang <spiderman_h@163.com>
2024-06-13 21:33:24 +0800

    初始化功能

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AIComponents-Generator
-AI驱动的前端UI组件生成器，它可以帮助您生成Next.js Tailwind CSS UI组件
+## AI驱动的前端UI组件生成器，它可以帮助您生成Next.js Tailwind CSS UI组件
+
+
+- 使用Next.js、TypeScript和Tailwind CSS构建UI组件生成器Web应用程序。
+- 使用CopilotKit将AI功能集成到UI组件生成器中。
+- 集成嵌入式代码编辑器，以对生成的代码进行更改。
---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotSidebar } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
+import Header from "./components/Header";
+import CodeTutorial from "./components/CodeTutorial";
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <CopilotKit runtimeUrl="/api/copilotkit">
+                <CopilotSidebar
+                    instructions='Help the user generate code. Ask the user if to generate its tutorial.'
+                    defaultOpen={true}
+                    labels={{
+                        title: "Code & Tutorial Generator",
+                        initial: "Hi! 👋 I can help you generate code and its tutorial.",
+                    }}
+                >
+                    <CodeTutorial />
+                </CopilotSidebar>
+            </CopilotKit>
+            <CodeTutorial />
+        </>
+    );
+}
